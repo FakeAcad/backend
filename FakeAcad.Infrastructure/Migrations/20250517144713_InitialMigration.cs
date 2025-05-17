6034c291e837f4722326aa9c928a7fedc0fba382
Earthbert <daraban_timotei@tutanota.com>
2025-05-17 17:52:49 +0300

    Update Migration
    Signed-off-by: Earthbert <daraban_timotei@tutanota.com>

---
+++ FakeAcad.Infrastructure/Migrations/20250517144713_InitialMigration.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FakeAcad.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:PostgresExtension:unaccent", ",,");
+
+            migrationBuilder.CreateTable(
+                name: "Professor",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FirstName = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    LastName = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Position = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Professor", x => x.Id);
+                    table.UniqueConstraint("AK_Professor_FirstName_LastName", x => new { x.FirstName, x.LastName });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "University",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_University", x => x.Id);
+                    table.UniqueConstraint("AK_University_Name", x => x.Name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Email = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Password = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Role = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                    table.UniqueConstraint("AK_User_Email", x => x.Email);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProfessorUniversity",
+                columns: table => new
+                {
+                    ProfessorsId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UniversitiesId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProfessorUniversity", x => new { x.ProfessorsId, x.UniversitiesId });
+                    table.ForeignKey(
+                        name: "FK_ProfessorUniversity_Professor_ProfessorsId",
+                        column: x => x.ProfessorsId,
+                        principalTable: "Professor",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProfessorUniversity_University_UniversitiesId",
+                        column: x => x.UniversitiesId,
+                        principalTable: "University",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Article",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Description = table.Column<string>(type: "character varying(1024)", maxLength: 1024, nullable: false),
+                    Content = table.Column<string>(type: "character varying(4096)", maxLength: 4096, nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Article", x => x.Id);
+                    table.UniqueConstraint("AK_Article_Title", x => x.Title);
+                    table.ForeignKey(
+                        name: "FK_Article_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ArticleProfessor",
+                columns: table => new
+                {
+                    ArticlesId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProfessorsId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArticleProfessor", x => new { x.ArticlesId, x.ProfessorsId });
+                    table.ForeignKey(
+                        name: "FK_ArticleProfessor_Article_ArticlesId",
+                        column: x => x.ArticlesId,
+                        principalTable: "Article",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ArticleProfessor_Professor_ProfessorsId",
+                        column: x => x.ProfessorsId,
+                        principalTable: "Professor",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ArticleUniversity",
+                columns: table => new
+                {
+                    ArticlesId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UniversitiesId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArticleUniversity", x => new { x.ArticlesId, x.UniversitiesId });
+                    table.ForeignKey(
+                        name: "FK_ArticleUniversity_Article_ArticlesId",
+                        column: x => x.ArticlesId,
+                        principalTable: "Article",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ArticleUniversity_University_UniversitiesId",
+                        column: x => x.UniversitiesId,
+                        principalTable: "University",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Article_UserId",
+                table: "Article",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArticleProfessor_ProfessorsId",
+                table: "ArticleProfessor",
+                column: "ProfessorsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArticleUniversity_UniversitiesId",
+                table: "ArticleUniversity",
+                column: "UniversitiesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProfessorUniversity_UniversitiesId",
+                table: "ProfessorUniversity",
+                column: "UniversitiesId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ArticleProfessor");
+
+            migrationBuilder.DropTable(
+                name: "ArticleUniversity");
+
+            migrationBuilder.DropTable(
+                name: "ProfessorUniversity");
+
+            migrationBuilder.DropTable(
+                name: "Article");
+
+            migrationBuilder.DropTable(
+                name: "Professor");
+
+            migrationBuilder.DropTable(
+                name: "University");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
